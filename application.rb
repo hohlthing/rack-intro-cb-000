b56f8b3e88042311f7005d3e3638ea4e05e9680f
michael hohl <mhohl@rocketmail.com>
2018-05-08 18:15:43 +0000

    automatically backed up by learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, my name is"
+    resp.write "Hello, my name is h o h l t h i n g"
     resp.finish
   end
 
